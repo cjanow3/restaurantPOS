0cf4cdce4150ce86eaee6cf93d76df0c19c896c0
cjanow3 <cjanow3@uic.edu>
2017-06-14 14:04:56 -0500

    Introduced email of data feature
    - last update before attempt at switching views to iphone 5 and putting app on iphone for testing email

--- EndOfDayViewController.swift
+++ EndOfDayViewController.swift
@@ -7,9 +7,9 @@
 //
 
 import UIKit
+import MessageUI
 
-
-class EndOfDayViewController: UIViewController {
+class EndOfDayViewController: UIViewController, MFMailComposeViewControllerDelegate {
     
     
     //MARK: Reset day
@@ -21,6 +21,80 @@ class EndOfDayViewController: UIViewController {
     
     
     
+    @IBAction func emailCurrentData(_ sender: Any)
+    {
+        //fetch data that will be emailed
+        let orders = restaurantController.fetchOrders()
+        
+        //format current date so we know what day the orders are from
+        let date = Date()
+        let formatter = DateFormatter()
+        let filenameEnding = "_TaylorOrderData.csv"
+    
+        formatter.dateFormat = "MM.dd.yyyy"
+        var filename = formatter.string(from: date)
+        
+        filename.append(filenameEnding)
+        
+        //print (filename)
+        
+        
+        let path = NSURL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent(filename)
+        
+        var csvText = "Num Deliveries, Total $ Deliveries, Num Pickups, Total $ Pickups\n"
+        
+        var numD = 0
+        var numP = 0
+        var monD = 0.0
+        var monP = 0.0
+        
+        for ord in orders
+        {
+            if (ord.pickup)
+            {
+                numP += 1
+                monP += ord.getPrice()
+            } else {
+                
+                numD += 1
+                monD += ord.getPrice()
+            }
+        
+        }
+        
+        let newLine = "\(numD), \(monD), \(numP), \(monP)"
+        csvText.append(newLine)
+        
+        //print(csvText)
+        
+        do {
+            
+            try csvText.write(to: path!, atomically: true, encoding: String.Encoding.utf8)
+            
+        } catch {
+            
+            print("Failed to create file")
+            print("\(error)")
+        }
+        
+        
+        if MFMailComposeViewController.canSendMail() {              //checking to see if it is possible to check mail
+            let emailController = MFMailComposeViewController()     //email view controller init
+            emailController.mailComposeDelegate = self              //setting delegate to self
+            emailController.setToRecipients(["chrisjanowski@hotmail.com"])
+            emailController.setSubject("Email data for \(date)")
+            emailController.setMessageBody("See attached .csv file for the excel data", isHTML: false)
+            
+            emailController.addAttachmentData(NSData(contentsOf: path!)! as Data, mimeType: "text/csv", fileName: "Orders.csv")
+            
+            present(emailController, animated: true, completion: nil)
+        }
+        
+        
+        func mailComposeController(controller: MFMailComposeViewController, didFinishWithResult result: MFMailComposeResult, error: NSError?) {
+            controller.dismiss(animated: true, completion: nil)
+        }
+    }
 
     
     
